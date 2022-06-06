bdbf5733e56d5132e67056829ad7a950aa6aa68c
rutger <rtr@mailbox.org>
2022-06-06 15:08:29 +0200

    Add support for metadata

--- cbs_pandas/core/dataset.py
+++ cbs_pandas/core/dataset.py
@@ -24,6 +24,7 @@ class Dataset(BaseModel):
     observation_count: int
     dataset_type: str
     _data: pd.DataFrame = None
+    _metadata: dict = None
 
     class Config:
         alias_generator = to_camel
@@ -41,11 +42,26 @@ class Dataset(BaseModel):
     def df(self):
         self._ensure_have_data()
         return self._data
+        
+    @property
+    def metadata(self):
+        self._ensure_have_metadata()
+        return self._metadata
 
     def _ensure_have_data(self):
         if self._data is None:
             target_url = f"https://beta-odata4.cbs.nl/CBS/{self.identifier}/Observations"
             self._data = self._get_odata(target_url)
+    
+    def _ensure_have_metadata(self):
+        if self._metadata is None:
+            metadata = {}
+            target_url = f"https://beta-odata4.cbs.nl/CBS/{self.identifier}"
+
+            for item in requests.get(target_url).json()['value']:
+                if item['name'] not in ['Properties', 'Observations']:
+                    metadata[item['name']] = requests.get(target_url + '/' + item['name']).json()
+            self._metadata = metadata
 
     @staticmethod
     def _get_odata(target_url):
